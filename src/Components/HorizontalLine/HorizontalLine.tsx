7b5ca8c42b5754841fd80dbd97b05f87fc997dc3
Kinga Boros <kinga.boros20@gmail.com>
2021-06-06 13:19:51 +0300

    Adding the horizontal line component

---
+++ src/Components/HorizontalLine/HorizontalLine.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HorizontalLine.scss';
+
+const HorizontalLine = () => {
+  return <div className="horizontal-line"></div>;
+};
+
+export default HorizontalLine;
