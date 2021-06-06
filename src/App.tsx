cafa453278a9a55735895bf0ff4f3d94b057b5c2
Kinga Boros <kinga.boros20@gmail.com>
2021-06-06 14:35:00 +0300

    Styling GoodHands component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './Components/Header/Header';
+import GoodHands from './Components/GoodHands/GoodHands';
 
 import './App.scss';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App-layer">
       <div className="container-fluid">
         <Header />
+        <GoodHands />
       </div>
     </div>
   );
---
+++ src/Components/GoodHands/GoodHands.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaLongArrowAltRight } from 'react-icons/fa';
+import HorizontalLine from '../HorizontalLine/HorizontalLine';
+
+import img from '../../Images/Content-Image.png';
+import './GoodHands.scss';
+
+const GoodHands = () => {
+  return (
+    <div className="good-hands__wrapper">
+      <div class="container-fluid">
+        <div class="row good-hands__img">
+          <div className="col-md-6">
+            <img src={img} alt="people speaking with agent" />
+          </div>
+
+          <div className="col-md-6 good-hands__text">
+            <HorizontalLine />
+            <div className="good-hands__title">You're in good hands</div>
+            <p className="good-hands__description">
+              Torquatos nostros? quos dolores eos, qui dolorem ipsum per se
+              texit, ne ferae quidem se repellere, idque instituit docere sic:
+              omne animal, simul atque integre iudicante itaque aiunt hanc quasi
+              involuta aperiri, altera occulta quaedam et voluptatem accusantium
+              doloremque.
+            </p>
+            <button className="btn btn-dark good-hands__button">
+              Learn More{' '}
+              <FaLongArrowAltRight className="good-hands__button__arrow" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GoodHands;
