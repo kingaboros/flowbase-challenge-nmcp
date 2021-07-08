675af39bfbb64f17302bcf8a8aa9e224a2d9d51a
Kinga Boros <kinga.boros20@gmail.com>
2021-07-08 11:07:25 +0300

    adding login page component and styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.scss';
 import Homepage from './screens/HomePage/Homepage';
+import LoginPage from './screens/LoginPage';
 
 const App = () => {
   return (
     <div className="App-layer">
+      <LoginPage />
       <Homepage />
     </div>
   );
